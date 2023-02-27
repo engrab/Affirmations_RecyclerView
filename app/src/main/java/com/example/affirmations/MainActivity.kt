041ebcbedcb2a1403e919c72758cabaf65e4ce4d
engrab <engrbilawalhussain@gmail.com>
2023-02-27 12:10:33 +0500

    Initial commit

---
+++ app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.affirmations
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.affirmations.adapters.ItemAdapter
+import com.example.affirmations.data_classes.Datasource
+import com.example.affirmations.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.recyclerView.adapter = ItemAdapter(this, Datasource().loadAffirmation())
+        binding.recyclerView.setHasFixedSize(true)
+
+    }
+}
---
+++ app/src/main/java/com/example/affirmations/data_classes/Affirmation.kt
@@ -0,0 +1,4 @@
+package com.example.affirmations.data_classes
+
+data class Affirmation(val stringResourceId: Int) {
+}
